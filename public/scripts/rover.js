5547eb79689f16fbeda7248db963dc32775681ff
Melody Yun Chen <Melodyyunc@gmail.com>
2018-05-08 16:59:03 -0400

    basic framework

--- public/scripts/rover.js
+++ public/scripts/rover.js
@@ -33,10 +33,41 @@ rover.getQuote = function () {
         console.log(res);
     });
 };
+//parallax effect for scroll
+//forground moves faster than background
+
+rover.randomNum = function () {
+    return Math.floor(Math.random() * 2);
+};
+
+console.log(rover.randomNum());
+
+rover.chooseAPI = function (num) {
+    if (num === 0) {
+        console.log('quote');
+        rover.getQuote();
+        //call quote api 
+        //display bubble above the rover
+        //make quote last 5 seconds and then disappear
+    } else {
+        console.log('nasa');
+
+        //call nasa api
+        // display full image 
+        //include exit button to close image
+    }
+};
+
+rover.eventRoverClick = function () {
+    $('.rover-img').on('click', function () {
+        rover.chooseAPI(rover.randomNum());
+    });
+};
 
 rover.init = function () {
+    rover.eventRoverClick();
+    //nasa called in init because it takes awhile to load and only needs to be called once
     rover.getNasa();
-    rover.getQuote();
 };
 
 $(rover.init());

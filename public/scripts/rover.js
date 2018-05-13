7f6fcc1059d7a08ee7532f85b09decfae5fec7ed
Melody Yun Chen <Melodyyunc@gmail.com>
2018-05-13 14:12:53 -0400

    social media icons

--- public/scripts/rover.js
+++ public/scripts/rover.js
@@ -3,6 +3,11 @@
 var rover = {};
 
 rover.roverApiKey = 'lcjdv0yXDikxF5uomOk79VCAgZ1lt1XtEGLxIFmC';
+//rover.introMusic = new Audio('../audio/intro.mp3');
+//rover.bgMusic = new Audio('space.mp3');
+// rover.bgMusic = new Howl({
+//     src: ['space.mp3']
+// });
 
 //----------
 // API Calls
@@ -135,6 +140,7 @@ rover.introSubmit = function () {
     $('#chooseRover').on('change', function () {
         rover.roverChoice = $(this).val();
         rover.chooseRover();
+        //rover.bgMusic.play();
         intro.hide();
         main.show();
     });
